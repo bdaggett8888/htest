e405236216fff64a31df313b9a98d11a0512e875
bdaggett8888 <89952390+bdaggett8888@users.noreply.github.com>
2021-11-19 20:07:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+app.use(express.static('public'));
+
+app.use(express.json()); // lets you handle JSON input
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
